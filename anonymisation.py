20173e7cffd5988fc14a7458155a274fe4d9c3fb
miharcan <mihael.arcan@gmail.com>
2023-01-05 13:10:06 +0000

    if translation, exceptions

--- anonymisation.py
+++ anonymisation.py
@@ -3,7 +3,7 @@ from flair.models import SequenceTagger
 import nltk
 import re
 import truecase
-import spacy
+from langdetect import detect
 import ctranslate2
 import sentencepiece as spm
 import urllib.request
@@ -34,6 +34,14 @@ translator = ctranslate2.Translator("ct2_model/")
 sp_estl = spm.SentencePieceProcessor("ct2_model/spm.estl.model")
 sp_en = spm.SentencePieceProcessor("ct2_model/spm.en.model")
 
+def lang_id(string: str):
+    """
+    language idetigfcation using ???
+    :param string: string used to detected language
+    :return: detected language id
+    """
+    detect_language = detect(string)
+    return detect_language
 
 def spacy_ano(string: str):
     """
@@ -81,6 +89,17 @@ def read_company_thesaurus(thesaurus: str):
         for line in thesaurus_file.readlines():
             dicti[line.replace('\n', '').lower()] = "COMP"
 
+def do_translation(string: str):
+    """
+    translation module
+    :param string: string to be translated
+    :return: translated text
+    """
+    input_tokens = sp_estl.encode(string, out_type=str)
+    results = translator.translate_batch([input_tokens])
+    output_tokens = results[0].hypotheses[0]
+    output_text = sp_en.decode(output_tokens)
+    return output_text
 
 def main_ano(string: str, thesaurus: str):
     """
@@ -96,23 +115,24 @@ def main_ano(string: str, thesaurus: str):
     if m is not None:
         string = string + "."
 
-    input_tokens = sp_estl.encode(string, out_type=str)
-    results = translator.translate_batch([input_tokens])
-    output_tokens = results[0].hypotheses[0]
-    output_text = sp_en.decode(output_tokens)
+    lowerstring = ""
 
+    if re.search('[a-zA-Z]', string):
+        output_text = string
+        if ((lang_id(string) != "en")): output_text = (do_translation(string))
 
-    string_tc = truecase.get_true_case(output_text)
-    spacy_ano(string_tc)
-    nltk_ano(string_tc)
-    flair_ano(string_tc)
+        string_tc = truecase.get_true_case(output_text)
 
-    lowerstring = string_tc.lower()
+        spacy_ano(string_tc)
+        nltk_ano(string_tc)
+        flair_ano(string_tc)
 
-    for key in dicti:
-        lowerstring = re.sub(r"\b%s\b" % key, dicti[key], lowerstring)
+        lowerstring = string_tc.lower()
 
-    pattern = r'[0-9]'
-    lowerstring = re.sub(pattern, "X", lowerstring)
+        for key in dicti:
+            lowerstring = re.sub(r"\b%s\b" % key, dicti[key], lowerstring)
+
+        pattern = r'[0-9]'
+        lowerstring = re.sub(pattern, "X", lowerstring)
 
     return lowerstring
