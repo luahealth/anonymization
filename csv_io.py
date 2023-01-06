6b0b61ca340e87c04c5e89e53139163199640f1e
Fionn Delahunty <Fionndelahunty@gmail.com>
2023-01-06 17:14:30 +0000

    fix: remove tqdm

--- csv_io.py
+++ csv_io.py
@@ -1,7 +1,6 @@
 from csv import reader
 from anonymisation import main_ano
 from loguru import logger
-from tqdm import tqdm
 
 import sys
 
@@ -11,11 +10,12 @@ thesaurus_path = "thesaurus.txt"
 
 with open(filepath) as file:
     logger.success(f'Read file {filepath}')
+    logger.info('Anonymising text, this may take some time')
     new_file_path = filepath+".anonymized.tsv"
     with open(new_file_path, "w") as output:
         csv_reader = reader(file)
         next(csv_reader)
-        for row in tqdm(csv_reader):
+        for row in csv_reader:
             message = " ".join(row[1:])
             output.write(row[0]+"\t"+message+"\t"+main_ano(message, thesaurus_path)+"\n")
     logger.success(f'Exported to file: {new_file_path}')
