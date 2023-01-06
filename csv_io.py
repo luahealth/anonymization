c81e592ddd24f556c8ce66ada17d871023b0e5a7
Fionn Delahunty <Fionndelahunty@gmail.com>
2023-01-06 17:10:48 +0000

    feat: add tqdm

--- csv_io.py
+++ csv_io.py
@@ -1,6 +1,8 @@
 from csv import reader
 from anonymisation import main_ano
 from loguru import logger
+from tqdm import tqdm
+
 import sys
 
 filepath = sys.argv[1]
@@ -13,7 +15,7 @@ with open(filepath) as file:
     with open(new_file_path, "w") as output:
         csv_reader = reader(file)
         next(csv_reader)
-        for row in csv_reader:
+        for row in tqdm(csv_reader):
             message = " ".join(row[1:])
             output.write(row[0]+"\t"+message+"\t"+main_ano(message, thesaurus_path)+"\n")
     logger.success(f'Exported to file: {new_file_path}')
